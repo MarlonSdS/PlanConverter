1ae249279ee5a7d82b047edbd81b456592512035
Marlon Santana dos Santos <marlonfms2012@gmail.com>
2021-09-26 10:38:37 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const fs = require("fs");
+var Reader = require("./Reader");
+var Processor = require("./Processor")
+var Table = require("./Table");
+var HtmlParser = require("./HtmlParser");
+var Writer = require("./Writer");
+var PDFWriter = require("./PDFWriter");
+
+var leitor = new Reader();
+var escritor = new Writer();
+
+async function main(){
+    var dados = await leitor.Read("./projects.csv");
+    var dadosProcessados = Processor.Process(dados);
+    var table = new Table(dadosProcessados);
+    var html = await HtmlParser.Parse(table)
+
+    escritor.Write(Date.now()+".html", html)
+    PDFWriter.WritePDF(Date.now()+".PDF", html)
+}
+
+main();
